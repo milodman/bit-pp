25a45ecc20095dd5835a61ec63ef7aa14c2c1e7c
BIT Student <classroom@bgit.rs>
2018-02-02 15:24:25 +0100

    First commit

---
+++ index.js
@@ -0,0 +1,8 @@
+var num = 36;
+    if (typeof num === "number"){
+        if (num>9 && num<100){
+            console.log (63);
+        } else {
+            console.log ("Error!")
+        }
+    } 
