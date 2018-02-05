7bd65c77760d79724c81847785a59b488e85d45b
BIT Student <classroom@bgit.rs>
2018-02-05 11:41:29 +0100

    zadatak5

---
+++ switch/zadata5.js
@@ -0,0 +1,27 @@
+var month =1;
+
+switch (month) {
+    case 1:
+    case 2:
+    case 3:
+        console.log("Zima")
+        break;
+    case 4:
+    case 5:
+    case 6:
+        console.log("Prolece");
+        break;
+    case 7:
+    case 8:
+    case 9:
+        console.log("Leto")
+        break;
+    case 10:
+    case 11:
+    case 12:
+        console.log ("Jesen")
+    break;
+    default:
+        console.log("input must be number between 1-12");
+        break;
+}
