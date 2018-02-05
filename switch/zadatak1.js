c83fa2c1a090c8cb06410bea9eb3852afd5a27e9
BIT Student <classroom@bgit.rs>
2018-02-05 11:02:29 +0100

    switchStatement-zad1

---
+++ switch/zadatak1.js
@@ -0,0 +1,29 @@
+var day = 2;
+
+switch (day) {
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;
+    case 3:
+        console.log("Wednseday");
+        break;
+    case 4:
+        console.log("Thrusday");
+        break;
+    case 5:
+        console.log("Friday");
+        break;
+    case 6:
+        console.log("Saturday");
+        break;
+    case 7:
+        console.log("Sunday");
+        break;
+    default:
+        console.log("Try again");
+        break;
+
+}
