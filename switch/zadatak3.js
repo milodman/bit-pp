cdf1e1a495e6698d6bdfc3d168bb26903c870832
BIT Student <classroom@bgit.rs>
2018-02-05 11:18:11 +0100

    switchStatement-zadatak3

---
+++ switch/zadatak3.js
@@ -0,0 +1,19 @@
+var dan = 61;
+
+switch (dan) {
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+        console.log("It\' weekday");
+        break;
+    case 6:
+    case 7:
+        console.log("it\'s weekend");
+        break;
+    default:
+        console.log("Input must be num between 1 and 7");
+        break;
+
+}
