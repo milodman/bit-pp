0c678d600d5733accfc677c0debeb2b82f98be7a
BIT Student <classroom@bgit.rs>
2018-02-14 17:10:45 +0100

    iiefs

---
+++ functions/iiefs/practice.js
@@ -0,0 +1,41 @@
+"use strict";
+
+/*var k = (  function (a,b) {
+    var sum = a+b;
+    console.log(sum);
+    
+}
+
+)(3,4);*/
+/*
+function outer(param) {
+    function inner(theinput) {   
+        return theinput * 2;
+        
+    }
+    return 'The result is ' + inner(param);
+ }
+ */
+//  function a() {
+//     console.log('A!');
+//     return function () {
+//         console.log('B!');
+        
+//     };
+//  }
+ 
+//  console.log(a()());
+
+
+// function a() {
+//     console.log('A!');
+//     return function () {
+//         console.log('B!');
+//         // return 1;
+//     };
+//  }
+//  var bigA = a();
+// //  bigA();
+// console.log(bigA());
+
+
