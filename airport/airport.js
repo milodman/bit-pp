4ce02e442949814d8151d976bd688133e7cf2db4
BIT Student <classroom@bgit.rs>
2018-02-26 17:56:29 +0100

    objects practice fixed

--- airport/airport.js
+++ airport/airport.js
@@ -6,22 +6,22 @@
             this.name = name;
             this.surname = surname;
 
-            this.getData = function () {
-                return this.name + " " + this.surname;
-            }
         }
-// function person
+        Person.prototype.getData = function () {
+            return this.name + " " + this.surname;
+        }
+        // function person
 
 
         function Seat(number, category) {
             this.number = number || Math.round(Math.random() * (100 - 10) + 10);
             this.category = category || "e";
 
-            this.getData = function () {
-                return this.number + ", " + this.category.toUpperCase();
-            }
         }
-// function seat
+        Seat.prototype.getData = function () {
+            return this.number + ", " + this.category.toUpperCase();
+        }
+        // function seat
 
 
         function Passenger(name, surname, number, category) {
@@ -30,11 +30,11 @@
             this.number = number || Math.round(Math.random() * (100 - 10) + 10);
             this.category = category || ("e");
 
-            this.getData = function (seat, person) {
-                return this.number + ", " + this.category.toUpperCase() + ", " + this.name + " " + this.surname;
-            }
         }
-// function passenger
+        Passenger.prototype.getData = function (seat, person) {
+            return this.number + ", " + this.category.toUpperCase() + ", " + this.name + " " + this.surname;
+        }
+        // function passenger
 
 
         function Flight(relation, date) {
@@ -44,70 +44,70 @@
             this.month = this.date.getMonth() + 1;
             this.day = this.date.getDate();
             this.listOfPassengers = [];
-            this.setDate = function () {
-                return this.day + "." + this.month + "." + this.year + ".";
-            }
-            this.getData = function () {
-                return this.setDate() + " " + this.relation;
-            }
-            this.addPassenger = function (passenger) {
-                this.listOfPassengers.push(passenger);
-            }
+        }
+        Flight.prototype.setDate = function () {
+            return this.day + "." + this.month + "." + this.year + ".";
+        }
+        Flight.prototype.getData = function () {
+            return this.setDate() + " " + this.relation;
+        }
+        Flight.prototype.addPassenger = function (passenger) {
+            this.listOfPassengers.push(passenger);
         }
 
         function Airport() {
             this.name = "Nikola Tesla";
             this.listOfFlights = [];
 
-            this.addFlight = function (flight) {
-                this.listOfFlights.push(flight);
-            }
 
-            this.getData = function () {
-                var totalPassengers = 0;
-                var output1 = "";
-                var output2 = "";
-                for (var i = 0; i < this.listOfFlights.length; i++) {
-                    var flight = this.listOfFlights[i];
-                    output1 += "\t" + this.listOfFlights[i].getData() + "\n";
-                    totalPassengers += flight.listOfPassengers.length
-                    for (var j = 0; j < this.listOfFlights[i].listOfPassengers.length; j++) {
-                        var passenger = this.listOfFlights[i].listOfPassengers[j]
-                        output1 += "\t\t" + passenger.getData() + "\n";
-                       
-                    }
+        }
+        Airport.prototype.addFlight = function (flight) {
+            this.listOfFlights.push(flight);
+        }
+        Airport.prototype.getData = function () {
+            var totalPassengers = 0;
+            var output1 = "";
+            var output2 = "";
+            for (var i = 0; i < this.listOfFlights.length; i++) {
+                var flight = this.listOfFlights[i];
+                output1 += "\t" + this.listOfFlights[i].getData() + "\n";
+                totalPassengers += flight.listOfPassengers.length
+                for (var j = 0; j < this.listOfFlights[i].listOfPassengers.length; j++) {
+                    var passenger = this.listOfFlights[i].listOfPassengers[j]
+                    output1 += "\t\t" + passenger.getData() + "\n";
+
                 }
-                var output = "Airport: " + this.name + ", total passengers: " + totalPassengers + "\n";
-
-
-                return output + output1;
             }
+            var output = "Airport: " + this.name + ", total passengers: " + totalPassengers + "\n";
+
+
+            return output + output1;
         }
         // function flight
 
         function createFlight(relation, date) {
             return new Flight(relation, date);
         }
-// create flight
+        // create flight
 
         function createPassenger(name, surname, number, category) {
             return new Passenger(name, surname, number, category);
         }
-// create passenger
+        // create passenger
 
         var passenger1 = createPassenger("Jessica", "Alba", 14, "b");
         var passenger2 = createPassenger("Julia", "Roberts", 11, "e");
         var passenger3 = createPassenger("Matt", "Damon");
         var passenger4 = createPassenger("Robbie", "Williams", 1);
-        var passenger5 = createPassenger("Mornar", "Popaj" )
-// create passenger
+        var passenger5 = createPassenger("Mornar", "Popaj")
+        // create passenger
 
         var airport1 = new Airport();
-// instanceof airport
+        // instanceof airport
 
         var flight1 = createFlight("Belgrade - Paris", "2018-04-05");
         var flight2 = createFlight("Barcelona - Belgrade", "2018-04-12");
-// create flight
+        // create flight
 
         flight1.addPassenger(passenger1);
         flight1.addPassenger(passenger2);
--- shopinglist/shoping.js
+++ shopinglist/shoping.js
@@ -1,4 +1,4 @@
-use strict";
+"use strict ";
 
 (function () {
     function Product(name, price, expdate) {
@@ -6,72 +6,70 @@ use strict";
         this.name = name;
         this.price = price;
         this.expdate = expdate;
-        this.productcode = function () {
-            var firstletter = this.name.charAt(0);
-            var lastletter = this.name.charAt(name.length - 1);
-            return firstletter.toUpperCase() + lastletter.toUpperCase() + this.id;
-        }
-        this.getInfo = function () {
-            return this.productcode() + ", " + this.name + ", " + this.price + this.expdate;
-        }
+    }
+    Product.prototype.productcode = function () {
+        var firstletter = this.name.charAt(0);
+        var lastletter = this.name.charAt(this.name.length - 1);
+        return firstletter.toUpperCase() + lastletter.toUpperCase() + this.id;
+    }
+    Product.prototype.getInfo = function () {
+        return this.productcode() + ", " + this.name + ", " + this.price + this.expdate;
     }
 
 
     function ShoppingBag() {
         this.listOfProducts = [];
-        this.addProduct = function (product) {
-            var date = new Date();
-            if (Date.parse(product.expdate) > Date.parse(date)) {
-                this.listOfProducts.push(product);
-            }
+    }
+    ShoppingBag.prototype.addProduct = function (product) {
+        var date = new Date();
+        if (Date.parse(product.expdate) > Date.parse(date)) {
+            this.listOfProducts.push(product);
+        }
 
+    }
+    ShoppingBag.prototype.averagePrice = function () {
+        var totalprice = 0;
+        var average = 0;
+        for (var i = 0; i < this.listOfProducts.length; i++) {
+            totalprice += this.listOfProducts[i].price;
         }
-        this.averagePrice = function () {
-            var totalprice = 0;
-            var average = 0;
-            for (var i = 0; i < this.listOfProducts.length; i++) {
-                totalprice += this.listOfProducts[i].price;
-            }
-            average = (totalprice / this.listOfProducts.length);
-            return average.toFixed(2);
+        average = (totalprice / this.listOfProducts.length);
+        return average.toFixed(2);
+    }
+    ShoppingBag.prototype.calculateTotalPrice = function () {
+        var totalprice = 0;
+        for (var i = 0; i < this.listOfProducts.length; i++) {
+            totalprice += this.listOfProducts[i].price;
         }
-        this.calculateTotalPrice = function () {
-            var totalprice = 0;
-            for (var i = 0; i < this.listOfProducts.length; i++) {
-                totalprice += this.listOfProducts[i].price;
-            }
-            return totalprice.toFixed(2);
+        return totalprice.toFixed(2);
+    }
+    ShoppingBag.prototype.getMostExpensive = function () {
+        var output = [];
+        var mostExpensiveItem;
+        for (var i = 0; i < this.listOfProducts.length; i++) {
+            output.push(this.listOfProducts[i].price);
         }
-        this.getMostExpensive = function () {
-            var output = [];
-            var mostExpensiveItem;
-            for (var i = 0; i < this.listOfProducts.length; i++) {
-                output.push(this.listOfProducts[i].price);
-            }
-            output.sort(function (a, b) {
-                return b - a;
-            })
-            mostExpensiveItem = output[0];
-            return mostExpensiveItem;
-        }
+        output.sort(function (a, b) {
+            return b - a;
+        })
+        mostExpensiveItem = output[0];
+        return mostExpensiveItem;
     }
 
     function PaymentCard() {
         this.accBalance = Math.round(899 * Math.random() + 100);
-        
-
-
 
     }
-    function checkoutAndBuy(shopinngBag,paymentCard) {
-        var missing = 0;
-        if( sb.calculateTotalPrice() <= cards1.accBalance){
-            return "The purchase is successful!!";
-        }   else {
-            missing = sb.totalprice-cards1.accBalance;
 
-            return   "You need " + missing + " dinars more on your account :-((((";
-        }  
+    function checkoutAndBuy(shopinngBag, paymentCard) {
+        var missing = 0;
+        if (sb.calculateTotalPrice() <= cards1.accBalance) {
+            return "The purchase is successful!!";
+        } else {
+            missing = sb.totalprice - cards1.accBalance;
+
+            return "You need " + missing + " dinars more on your account :-((((";
+        }
     }
 
 
